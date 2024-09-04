6cdb0f60cb54b1c7b7c03d49b65b6134ea0f1cbb
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2024-09-04 10:42:06 -0700

    Initial Upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# explorer
+# ANUMA EXPLORER
+
+This is the source code for Anuma Explorer [https://explorer.anuma.network](https://explorer.anuma.network) written in JS with React.JS library.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Development
+
+For developing this you need Node.JS and just npm install.
---
+++ src/components/BlockDAG.js
@@ -0,0 +1,126 @@
+import {faDiagramProject} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {useEffect, useState} from "react";
+import {getBlockdagInfo, getHashrateMax} from '../anuma-api-client';
+import {numberWithCommas} from "../helper";
+
+
+const BlockDAGBox = () => {
+
+    const [virtualDaaScore, setVirtualDaaScore] = useState(localStorage.getItem("cacheVirtualDaaScore") || "");
+    const [hashrate, setHashrate] = useState(localStorage.getItem("cacheHashrate"));
+    const [maxHashrate, setMaxHashrate] = useState(localStorage.getItem("cacheHashrateMax"));
+
+    const initBox = async () => {
+        const dag_info = await getBlockdagInfo()
+        const hashrateMax = await getHashrateMax()
+
+        setVirtualDaaScore(dag_info.virtualDaaScore)
+        localStorage.setItem("cacheVirtualDaaScore", dag_info.virtualDaaScore)
+        setHashrate((dag_info.difficulty * 2 / 1000000000000).toFixed(2))
+        localStorage.setItem("cacheHashrate", (dag_info.difficulty * 2 / 1000000000000).toFixed(2))
+        setMaxHashrate(hashrateMax.hashrate)
+        localStorage.setItem("cacheHashrateMax", hashrateMax.hashrate)
+    }
+
+    useEffect(() => {
+        initBox();
+        const updateInterval = setInterval(async () => {
+            const dag_info = await getBlockdagInfo()
+            setVirtualDaaScore(dag_info.virtualDaaScore)
+            setHashrate((dag_info.difficulty * 2 / 1000000000000).toFixed(2))
+            localStorage.setItem("cacheHashrate", (dag_info.difficulty * 2 / 1000000000000).toFixed(2))
+        }, 60000)
+        return (async () => {
+            clearInterval(updateInterval)
+        })
+    }, [])
+
+
+    useEffect((e) => {
+        document.getElementById('virtualDaaScore').animate([
+            // keyframes
+            {opacity: '1'},
+            {opacity: '0.6'},
+            {opacity: '1'},
+        ], {
+            // timing options
+            duration: 300
+        });
+    }, [virtualDaaScore])
+
+    useEffect((e) => {
+        document.getElementById('hashrate').animate([
+            // keyframes
+            {opacity: '1'},
+            {opacity: '0.6'},
+            {opacity: '1'},
+        ], {
+            // timing options
+            duration: 300
+        });
+    }, [hashrate])
+
+
+    function hashrateToStr(inHashrate) {
+        if (inHashrate / 1000 < 1000) {
+            return `${(inHashrate / 1000).toFixed(3)} PH/s`
+        } else {
+            return `${(inHashrate / 1000 / 1000).toFixed(3)} EH/s`
+        }
+
+    }
+
+    return <>
+        <div className="cardBox mx-0">
+            <table style={{fontSize: "1rem"}}>
+                <tr>
+                    <td colspan='2' className="text-center" style={{"fontSize": "4rem"}}>
+                        <FontAwesomeIcon icon={faDiagramProject}/>
+                        <div className="cardLight"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" className="text-center">
+                        <h3>BLOCKDAG INFO</h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="cardBoxElement">
+                        Network name
+                    </td>
+                    <td className="pt-1 text-nowrap">
+                        ANUMA MAINNET
+                    </td>
+                </tr>
+                <tr>
+                    <td className="cardBoxElement">
+                        Virtual DAA Score
+                    </td>
+                    <td className="pt-1 align-top" id="virtualDaaScore">
+                        {numberWithCommas(virtualDaaScore)}
+                    </td>
+                </tr>
+                <tr>
+                    <td className="cardBoxElement">
+                        Hashrate
+                    </td>
+                    <td className="pt-1" id="hashrate">
+                        {hashrateToStr(hashrate)}
+                    </td>
+                </tr>
+                <tr>
+                    <td className="cardBoxElement">
+                        Max Hashrate
+                    </td>
+                    <td className="pt-1" id="hashrate">
+                        {(maxHashrate / 1000 / 1000).toFixed(3)} EH/s
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </>
+}
+
+
+export default BlockDAGBox;
